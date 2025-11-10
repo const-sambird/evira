84d53ee38b6b7521198320c40d4baea0bf09252e
Sam Bird <sam@sambird.dev>
2025-11-09 18:02:08 -0600

    use sampler primitives

--- optim.py
+++ optim.py
@@ -10,6 +10,7 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
  
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
 
 from qiskit_aer import AerSimulator
 from qiskit_algorithms import QAOA
@@ -89,25 +90,95 @@ class QAOAOptimiser:
 
         self.pass_manager = generate_preset_pass_manager(optimization_level=3, backend=self.backend)
     
+    def get_qaoa_ansatz(self, operator):
+        qc = QAOAAnsatz(cost_operator=operator, reps=self.reps, initial_state=None)
+        qc.measure_active()
+        return qc
+    
     def optimise(self, qubo):
         sampler = Sampler(mode=self.backend, options={'default_shots': self.shots})
-        classical_optimiser = COBYLA()
-
-        qaoa = QAOA(sampler=sampler, optimizer=classical_optimiser, reps=self.reps, initial_point=None, transpiler=self.pass_manager)
 
         operator, offset = qubo
-        qaoa._check_operator_ansatz(operator)
-        operator = operator.apply_layout(qaoa.ansatz.layout)
+        ansatz = self.get_qaoa_ansatz(operator)
+        ansatz = self.pass_manager.run(ansatz)
 
-        result = qaoa.compute_minimum_eigenvalue(operator)
+        initial_gamma = np.pi
+        initial_beta = np.pi / 2
+        init_params = []
+
+        for _ in range(self.reps):
+            init_params.append(initial_beta)
+        for _ in range(self.reps):
+            init_params.append(initial_gamma)
+        
+        def qaoa_objective(params, ansatz, hamiltonian, sampler):
+            '''
+            Sampler-based approximation of an Estimator call for the
+            classical optimiser to get expectation values for the
+            trainable QAOA parameters
+            '''
+            print('qaoa_objective call')
+            # Bind parameters into the QAOA ansatz
+            circ = ansatz.assign_parameters(params)
+            
+            # Run circuit with Sampler primitive (hardware or simulator)
+            job = sampler.run([circ])
+            result = job.result()[0]
+            counts = result.data.meas.get_counts()
+            
+            # Convert to probabilities
+            total_shots = sum(counts.values())
+            probs = {bit: count / total_shots for bit, count in counts.items()}
+
+            # Compute expected energy
+            energy = 0.0
+            for bitstring, p in probs.items():
+                # Compute classical energy E(z)
+                z = np.array([int(b) for b in bitstring[::-1]])  # Qiskit uses little endian
+                E_z = 0.0
+                for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs):
+                    # Only diagonal Z terms matter
+                    label = pauli.to_label()[::-1]
+                    term_val = 1.0
+                    for bit, pauli_char in zip(z, label):
+                        if pauli_char == "Z":
+                            term_val *= (1 - 2 * bit)  # Z eigenvalues: +1 for |0>, -1 for |1>
+                        elif pauli_char == "I":
+                            term_val *= 1
+                        else:
+                            # X or Y terms vanish under Z measurement
+                            term_val = 0.0
+                            break
+                    E_z += coeff.real * term_val
+                energy += p * E_z
+
+            return energy.real
+
+        param_est = minimize(
+            qaoa_objective,
+            init_params,
+            args=(ansatz, operator, sampler),
+            method="COBYLA",
+            tol=1e-2,
+            options={'maxiter': 4}
+        )
+        opt_params = param_est.x
+        bound_circ = ansatz.assign_parameters(opt_params)
+
+        pub = (bound_circ,)
+        job = sampler.run([pub])
+
+        counts_int = job.result()[0].data.meas.get_int_counts()
+        shots = sum(counts_int.values())
+        final_distribution_int = {key: val / shots for key, val in counts_int.items()}
 
         solution = []
         
-        for bits, freq in result.eigenstate.items():
-            b = list(bits)
+        for bits, freq in final_distribution_int.items():
+            b = list(np.binary_repr(bits, width=ansatz.num_qubits))
             b.reverse()
             b = [int(bit) for bit in b]
-            solution.append((b, -freq))
+            solution.append((b[:self.n_indexes], -freq))
         
         solution = np.array(solution, dtype=[('sample', 'i1', (self.n_indexes,)), ('energy', '<f8')])
         solution = solution.view(np.recarray)
